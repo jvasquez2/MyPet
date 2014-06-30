cb2435b4ee8ddb74f9eca004857d8b031dc81915
jvasquez2 <juan_vasquez@bitbucket.org>
2014-06-30 16:53:20 -0500

    Commit inicial de nuestro super proyecto

---
+++ WebContent/WEB-INF/paginas/principal.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+
+
+<html>
+<head>
+
+<!--  <link rel="import" href="../polymers/principal.html"> -->
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>My Pet</title>
+</head>
+<body>
+
+<!-- <div><my-element></my-element></div> -->
+
+
+</body>
+</html>
---
+++ src/util/MyBatisUtil.java
@@ -0,0 +1,35 @@
+package util;
+
+
+
+import java.io.Reader;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+/*
+ *
+ * @author Administrador
+ */
+public class MyBatisUtil {
+    
+	 protected static final SqlSessionFactory FACTORY;
+	    
+	    static{
+	        try{
+	            Reader reader= Resources.getResourceAsReader(
+	             "mybatis-config.xml");
+	            
+	            FACTORY= new SqlSessionFactoryBuilder().build(reader,"clase");
+	            
+	        }catch(Exception e){
+	        	e.printStackTrace();
+	            throw new RuntimeException("Error: " + e);
+	        }
+	    }
+	 
+	    
+	    public static SqlSessionFactory getSqlSessionFactory(){
+	        return FACTORY;
+	    }
+}
